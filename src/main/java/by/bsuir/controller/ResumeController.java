e28e9492e37b6ed6a9eed0c9aa7cb0975218c11c
tatsiana <tatsiana.pivovarchick@gmail.com>
2018-04-08 17:35:37 +0300

    add resume controller

---
+++ src/main/java/by/bsuir/controller/ResumeController.java
@@ -0,0 +1,52 @@
+package by.bsuir.controller;
+
+import by.bsuir.entity.Resume;
+import by.bsuir.service.IResumeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("aspirant")
+public class ResumeController {
+    @Autowired
+    private IResumeService resumeService;
+
+    @GetMapping("resume")
+    public ResponseEntity<Resume> getArticleById(@RequestParam("id") String id) {
+        Resume resume = resumeService.getResumeById(Integer.parseInt(id));
+        return new ResponseEntity<Resume>(resume, HttpStatus.OK);
+    }
+
+    @GetMapping("all-resumes")
+    public ResponseEntity<List<Resume>> getAllResumes() {
+        List<Resume> list = resumeService.getAllResumes();
+        return new ResponseEntity<List<Resume>>(list, HttpStatus.OK);
+    }
+
+    @PostMapping("resume")
+    public ResponseEntity<Void> createResume(@RequestBody Resume resume, UriComponentsBuilder builder) {
+        resumeService.createResume(resume);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(builder.path("/resume?id={id}").buildAndExpand(resume.getId()).toUri());
+        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("resume")
+    public ResponseEntity<Resume> updateResume(@RequestBody Resume resume) {
+        resumeService.updateResume(resume);
+        return new ResponseEntity<Resume>(resume, HttpStatus.OK);
+    }
+
+    @DeleteMapping("resume")
+    public ResponseEntity<Void> deleteResume(@RequestParam("id") String id) {
+        resumeService.deleteResume(Integer.parseInt(id));
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+    }
+}
