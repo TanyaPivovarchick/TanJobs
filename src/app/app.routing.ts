252d3e3e951a666279839c3441e878e6f6facc12
tatsiana <tatsiana.pivovarchick@gmail.com>
2018-04-24 21:46:01 +0300

    add routing

---
+++ src/app/app.routing.ts
@@ -0,0 +1,12 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { StartPageComponent } from './components/start-page/start-page.component';
+import { LoginComponent } from './components/login/login.component';
+
+const appRoutes: Routes = [
+	{ path: '', component: StartPageComponent },
+    { path: 'login', component: LoginComponent },
+	{ path: '**', redirectTo: '' }
+];
+
+export const Routing = RouterModule.forRoot(appRoutes);
