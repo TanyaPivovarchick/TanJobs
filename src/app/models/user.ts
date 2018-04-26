d0caa1ff9812d005e3a85faf76d4ecad13ec957c
tatsiana <tatsiana.pivovarchick@gmail.com>
2018-04-26 04:00:56 +0300

    add user model

---
+++ src/app/models/user.ts
@@ -0,0 +1,18 @@
+export class User {
+
+    id: string;
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+
+    constructor() {
+    }
+
+    public static isNull(user: User): boolean {
+        return user.email === null &&
+            user.password === null &&
+            user.firstName === null &&
+            user.lastName === null;
+    }
+}
