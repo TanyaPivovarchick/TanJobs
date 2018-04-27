978dcc3065b868ed852f666b321a74b4f106a562
tatsiana <tatsiana.pivovarchick@gmail.com>
2018-04-28 01:25:19 +0300

    update database sql according data model changes

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,10 +1,6 @@
--- Table: aspirant_data
-CREATE TABLE IF NOT EXISTS aspirant_data (
+-- Table: user_data
+CREATE TABLE IF NOT EXISTS user_data (
   id                INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  surname           VARCHAR(45) NULL,
-  name              VARCHAR(45) NULL,
-  patronymic        VARCHAR(45) NULL,
-  email             VARCHAR(45) NULL,
   sex               VARCHAR(1)  NULL,
   education         MEDIUMTEXT  NULL,
   date_of_birth     DATE        NULL,
@@ -16,14 +12,16 @@ CREATE TABLE IF NOT EXISTS aspirant_data (
 )
 ENGINE = InnoDB;
 
--- Table: aspirant
-CREATE TABLE IF NOT EXISTS aspirant (
-  id               INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  email            VARCHAR(45) NOT NULL,
-  password         VARCHAR(60) NOT NULL,
-  aspirant_data_id INT         NOT NULL,
+-- Table: user
+CREATE TABLE user (
+  id           INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email        VARCHAR(30) NOT NULL,
+  password     VARCHAR(60) NOT NULL,
+  first_name   VARCHAR(30) NOT NULL,
+  last_name    VARCHAR(30) NOT NULL,
+  user_data_id INT,
 
-  FOREIGN KEY (aspirant_data_id) REFERENCES aspirant_data(id)
+  FOREIGN KEY (user_data_id) REFERENCES user_data(id)
 )
 ENGINE = InnoDB;
 
@@ -37,14 +35,16 @@ CREATE TABLE IF NOT EXISTS resume (
   skills           VARCHAR(150) NULL,
   salary           DECIMAL(7,2) NULL,
   number_of_view   INT          NULL,
-  aspirant_id      INT          NOT NULL,
+  user_id          INT          NOT NULL,
 
-  FOREIGN KEY (aspirant_id) REFERENCES aspirant(id)
+  FOREIGN KEY (user_id) REFERENCES user(id)
 )
 ENGINE = InnoDB;
 
 -- Insert data
-INSERT INTO aspirant_data VALUES (1, 'Super', 'Guy', 'Superov', 'user@gmail.com', 'm', 'BSUIR', '1985-10-12',
-                                  '(29)3456732', 'B2', '', 'Minsk', '');
-INSERT INTO aspirant VALUES (1, 'user@gmail.com', '$2a$11$uSXS6rLJ91WjgOHhEGDx..VGs7MkKZV68Lv5r1uwFu7HgtRn3dcXG', 1);
+INSERT INTO user_data VALUES (1, 'm', 'BSUIR', '1985-10-12', '(29)3456732', 'B2', '', 'Minsk', '');
+
+INSERT INTO user VALUES (1, 'user@gmail.com', '$2a$11$uSXS6rLJ91WjgOHhEGDx..VGs7MkKZV68Lv5r1uwFu7HgtRn3dcXG',
+                         'Super', 'Guy', 1);
+
 INSERT INTO resume VALUES (1, '2018-03-14 12:00:00', 'Car', 1, 1, 'Pascal', 500, 0, 1);
