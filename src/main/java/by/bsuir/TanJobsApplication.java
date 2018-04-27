d312766918daa11f0adcd4f1fdaeefa81d31a089
tatsiana <tatsiana.pivovarchick@gmail.com>
2018-04-28 01:17:08 +0300

    update config

--- src/main/java/by/bsuir/TanJobsApplication.java
+++ src/main/java/by/bsuir/TanJobsApplication.java
@@ -2,10 +2,19 @@ package by.bsuir;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TanJobsApplication {
+public class TanJobsApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(TanJobsApplication.class, args);
     }
 }
+
